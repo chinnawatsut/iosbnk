3278f8c697facff183d4661ad06e4b50849170ff
Chinnawat K <chinnawatsut@gmail.com>
2018-01-28 10:21:37 +0700

    Initial Commit

---
+++ bnk48todoTests/bnk48todoTests.swift
@@ -0,0 +1,35 @@
+//
+//  bnk48todoTests.swift
+//  bnk48todoTests
+//
+//  Created by Chinwat K. on 28/1/2561 BE.
+//
+
+import XCTest
+@testable import bnk48todo
+
+class bnk48todoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
